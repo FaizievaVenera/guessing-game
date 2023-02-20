0d770700863e81dfcdb4ca14e32a6be48e5cddf7
Faizieva Venera <v.faizyeva@gmail.com>
2023-02-20 10:40:47 +0300

    feat: added solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.currentIndex = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
+        return this.max = this.currentIndex;
 
     }
 
     greater() {
-
+        return this.min = this.currentIndex;
     }
 }
 
